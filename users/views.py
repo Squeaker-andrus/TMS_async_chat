336e2fd70223ff8cabd084876fc2cafba7802a44
Squeaker-andrus <118593365+Squeaker-andrus@users.noreply.github.com>
2023-03-20 23:43:48 +0300

    Add files via upload

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 import datetime
 import re
-
+import os
+import hashlib
 import aiohttp_jinja2
 from aiohttp import web
 from aiohttp.web_request import Request
@@ -19,16 +20,20 @@ class LogIn(web.View):
     async def post(self):
         data = await self.request.post()
         username = data.get('username', '').lower()
-
+        input_password = data.get('password', '')
         try:
             user = await User.get(username=username)
+
         except Exception as error:
             print(error)
             redirect(self.request, "login")
             return
 
         else:
-            self.login(user)
+            if input_password != hashlib.pbkdf2_hmac('sha256', input_password.encode('utf-8'), salt=User.get.password.key, 100000):
+                redirect(self.request, "login")
+            else:
+                self.login(user)
         return web.json_response({"user": user.id})
 
     def login(self, user: User):
@@ -53,6 +58,17 @@ class Register(web.View):
             return ""
         return username
 
+    async def check_password(self) -> dict:
+        """ Get password from post data, and check is correct """
+        data = await self.request.post()
+        row_password = data.get('password', '')
+        if not re.match(r'^[a-z]\w{0,9}$', row_password):
+            return {}
+        else:
+            salt = os.urandom(32)
+            password = hashlib.pbkdf2_hmac('sha256', row_password.encode('utf-8'), salt, 100000)
+        return {salt: password}
+
     def login(self, user: User):
         self.request.session["user_id"] = user.id
         self.request.session["time"] = str(datetime.datetime.now())
@@ -61,9 +77,10 @@ class Register(web.View):
 
     async def post(self):
         username = await self.check_username()
-        print('username', username)
+        password = await self.check_password()
+        print('username', username, password)
 
-        if not username:
+        if not username or not password:
             redirect(self.request, "register")
 
         try:
@@ -73,8 +90,9 @@ class Register(web.View):
         except:
             print("Пользователя нет!")
 
-        await User.create(username=username)
-        user = await User.get(username=username)
+        await User.create(username=username, password=password)
+        user = await User.get(username=username, password=password)
+
         self.login(user)
 
 
